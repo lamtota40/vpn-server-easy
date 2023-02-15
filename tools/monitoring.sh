df527bb867ad1da7b1f78c562c9be7982e7cc72d
lamtota40 <lamtota40@gmail.com>
2023-02-15 09:30:19 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "Time on server"
+echo "=====Time on server====="
 echo "Time =" date "+%H:%M:%S"
 echo "Date =" date "%d/%m/%y"
-echo "Uptime server =" uptime
+echo "Uptime server =" `uptime`
 
 FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
 SWAP=`free -m | grep "Swap" | awk '{print $3}'`

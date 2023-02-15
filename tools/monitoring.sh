4b354950a023bf984186a70c492f89915f752db8
lamtota40 <lamtota40@gmail.com>
2023-02-15 10:08:21 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -7,19 +7,10 @@ echo "Uptime ="
 
 echo "=====Detail server====="
 echo "Virtualization = " `grep -Eoc '(vmx|svm)' /proc/cpuinfo`
-echo "OS = " 'awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
+echo "OS = " `awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
 echo "CPU = "
 echo "RAM Usage = Mb | Free = Mb | Total ="
 echo "HHD Usage = Mb | Free = Mb | Total ="
 echo "Hostname = "
 echo "Ip Public= "
 echo "Ip Private= "
-
-FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
-SWAP=`free -m | grep "Swap" | awk '{print $3}'`
-UP=`uptime`
-
-echo $FREE
-echo $SWAP
-echo $UP
-echo "Created By : FreeDroid"

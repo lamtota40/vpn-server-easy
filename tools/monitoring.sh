5fb892e3f04e63459a60bdf02daa8a20dc7462a7
lamtota40 <lamtota40@gmail.com>
2023-02-15 09:22:25 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo -e "Waktu pada server"
+echo jam date "+%H:%M:%S"
+echo tanggal date "%d/%m/%y"
+
+
 FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
 SWAP=`free -m | grep "Swap" | awk '{print $3}'`
 UP=`uptime`

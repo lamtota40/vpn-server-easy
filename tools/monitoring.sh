cd49f50e172476b5b0209c82e826c423ec7d5823
lamtota40 <lamtota40@gmail.com>
2023-02-15 10:06:46 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-osv= `awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
-virt= `grep -Eoc '(vmx|svm)' /proc/cpuinfo`
-
 echo "=====Time on server====="
 echo "Time =" `date "+%H:%M:%S"`
 echo "Date =" `date "+%d/%m/%y"`
-echo "Uptime server ="
+echo "Uptime ="
 
 echo "=====Detail server====="
-echo "Virtualization = $virt"
-echo "OS = $osv"
-echo "CPU ="
-echo "RAM Usage= Mb Free= Mb Total="
-echo "HHD Usage= Mb Free= Mb Total="
+echo "Virtualization = " `grep -Eoc '(vmx|svm)' /proc/cpuinfo`
+echo "OS = " 'awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
+echo "CPU = "
+echo "RAM Usage = Mb | Free = Mb | Total ="
+echo "HHD Usage = Mb | Free = Mb | Total ="
+echo "Hostname = "
+echo "Ip Public= "
+echo "Ip Private= "
 
 FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
 SWAP=`free -m | grep "Swap" | awk '{print $3}'`

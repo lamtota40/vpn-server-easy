66ad8f61b3964e71977a18b4426b05f18760f701
lamtota40 <lamtota40@gmail.com>
2023-02-15 09:37:15 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+virt= `grep -Eoc '(vmx|svm)' /proc/cpuinfo`
+
 echo "=====Time on server====="
 echo "Time =" date "+%H:%M:%S"
 echo "Date =" date "%d/%m/%y"
 echo "Uptime server =" `uptime`
+
+echo "=====Detail server====="
+echo "virtualization = $virt "
 
 FREE=`free -m | grep "buffers/cache" | awk '{print $3}'`
 SWAP=`free -m | grep "Swap" | awk '{print $3}'`

25358f4a2e7627fab90fa7bf79ff2c4329ed0cef
lamtota40 <lamtota40@gmail.com>
2023-02-16 16:36:55 +0700

    Update setup-dropbear.sh

--- dropbear/setup-dropbear.sh
+++ dropbear/setup-dropbear.sh
@@ -2,3 +2,8 @@
 apt-get install dropbear -y
 sudo cp /etc/default/dropbear /etc/default/dropbear.bak
 nano /etc/default/dropbear
+
+NO_START=0
+DROPBEAR_EXTRA_ARGS="-p 109 -p 443 -p 143"
+
+#sudo apt-get purge dropbear -y

dd5562fd976083ca98f723e7ac9f4841fe6f640b
lamtota40 <lamtota40@gmail.com>
2023-02-16 18:07:14 +0700

    Update setup-dropbear.sh

--- dropbear/setup-dropbear.sh
+++ dropbear/setup-dropbear.sh
@@ -9,8 +9,9 @@ extra_port= "-p 80 -p 8080"
 
 sed -i -e 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i -e 's/DROPBEAR_PORT=22/DROPBEAR_PORT=$default_port/g' /etc/default/dropbear
-sed -i -e 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS=$extra_port/g' /etc/default/dropbear
-DROPBEAR_BANNER="/etc/issue.ssh"
+sed -i -e 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 80 -p 8080"/g' /etc/default/dropbear
+sed -i -e 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/issue.ssh"/g' /etc/default/dropbear
+
 systemctl enable dropbear
 systemctl start dropbear
 

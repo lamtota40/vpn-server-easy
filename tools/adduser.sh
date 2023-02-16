08064ca250d996c48fc6d552dc0a4d8e77b7b0c4
lamtota40 <lamtota40@gmail.com>
2023-02-16 22:05:25 +0700

    Update adduser.sh

--- tools/adduser.sh
+++ tools/adduser.sh
@@ -4,13 +4,13 @@ read -p "input username     : " username
 read -p "input password     : " password
 read -p "Expired (yyyy-mm-dd) : " expired
 egrep "^$username" /etc/passwd >/dev/null
-if [ $? -eq 0 ]; then
-     echo  "Not created Username $username Already!"
-     exit 1
-else
+#if [ $? -eq 0 ]; then
+     #echo  "Not created Username $username Already!"
+     #exit 1
+#else
      pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
      IP=`ifconfig venet0:0| awk 'NR==2 {print $2}'| awk -F: '{print $2}'`
      useradd -e $expired -d /home/$username -m -g users -p $pass -s /bin/false $username
      rm -r /home/$username
 		echo -e "===============\n$IP \nUser     : $username\nPassword : $password\nExpired  : $expired\n\n============\n"
-fi
+#fi

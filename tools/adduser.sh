3ba0604fba33e7954176962476a0cdfae1d95638
lamtota40 <lamtota40@gmail.com>
2023-02-16 21:59:31 +0700

    Update adduser.sh

--- tools/adduser.sh
+++ tools/adduser.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-clear
 
-if [ $(id -u) -eq 0 ]; then
-	read -p "Masukan username     : " username
-	read -p "Masukan password     : " password
-	read -p "Expired (yyyy-mm-dd) : " expired
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo  "Username $username sudah ada di VPS anda!"
-		exit 1
-	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		IP=`ifconfig venet0:0| awk 'NR==2 {print $2}'| awk -F: '{print $2}'`
-		useradd -e $expired -d /home/$username -m -g users -p $pass -s /bin/false $username
-		rm -r /home/$username
-		clear
-		echo "Created By : lamtota40"
-		echo -e "=========================================\n\nHost     : $IP\nPort     : 443,109,110,22,143,80\nUser     : $username\nPassword : $password\nExpired  : $expired\n\n=========================================\n"
-	fi
+
+read -p "input username     : " username
+read -p "input password     : " password
+read -p "Expired (yyyy-mm-dd) : " expired
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+     echo  "Not created Username $username Already!"
+     exit 1
 else
-	echo "Hanya root yang bisa menambah user ke system"
-	exit 2
+     pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+     IP=`ifconfig venet0:0| awk 'NR==2 {print $2}'| awk -F: '{print $2}'`
+     useradd -e $expired -d /home/$username -m -g users -p $pass -s /bin/false $username
+     rm -r /home/$username
+		echo -e "===============\nUser     : $username\nPassword : $password\nExpired  : $expired\n\n============\n"
 fi

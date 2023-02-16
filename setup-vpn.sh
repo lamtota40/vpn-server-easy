ddc9e9a5fd919721c229d34793ba4cf8f6d4ec0b
lamtota40 <lamtota40@gmail.com>
2023-02-16 16:15:19 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+echo "==================================="  
+echo " Author: github.com/lamtota40
+echo " Choose menu answer with number !"  
+echo "==================================="
+echo "1. Install Dropbear"
+echo "2. Install Shadowshock"
+echo "0. Quit"
+echo "==================================="
+read -p "choose [0-9]:" num
+     until [[ -z "$num" || "$num" =~ ^[0-9]$ ]]; do
+	echo "$num: invalid selection."
+	read -p "choose [0-9]: " num
+     done
+case $num in  
+    "1")  
+        wget -qO- https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/dropbear/setup-dropbear.sh | bash
+        ;;  
+    "2")  
+        echo "nothing"
+        ;;
+    "0")  
+        exit 1
+        ;;
+esac

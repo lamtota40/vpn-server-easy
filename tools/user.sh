ec21d649b923790e4c48e01ec188563983a6b6ec
lamtota40 <lamtota40@gmail.com>
2023-02-19 03:33:57 +0700

    Update user.sh

--- tools/user.sh
+++ tools/user.sh
@@ -5,7 +5,7 @@ read -p "input username     : " username
 read -p "input password     : " password
 
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -d /home/$username -m -g users -p $pass -s /bin/false $username
+useradd -p $pass -M username
 #rm -r /home/$username
 
 echo "selesai"

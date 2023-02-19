ba71e049b1c619c7651930ffa0a514ed46f117c9
lamtota40 <lamtota40@gmail.com>
2023-02-19 16:21:24 +0700

    Create setup-dropbear.sh

---
+++ SSH/dropbear/setup-dropbear.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! $(which sshd) ]; then
+    stat="Not Instaled"
+else
+    stat="Already Instaled"
+fi
+
+
+echo "Status :$stat"
+apt install dropbear -y
+cp /etc/default/dropbear /etc/default/dropbear.bak
+
+default_port= "443"
+extra_port= "-p 80 -p 8080"
+
+sed -i -e 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i -e 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i -e 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 80 -p 8080"/g' /etc/default/dropbear
+sed -i -e 's/DROPBEAR_BANNER=/DROPBEAR_BANNER="/etc/issue.ssh"/g' /etc/default/dropbear
+
+systemctl enable dropbear
+systemctl restart dropbear
+
+#unistall
+#sudo apt-get purge dropbear -y

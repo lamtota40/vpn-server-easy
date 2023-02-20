d108cbf80fe2fb31b9d4fe2931d74aedb417cac8
lamtota40 <lamtota40@gmail.com>
2023-02-20 15:57:10 +0700

    Update setup-dropbear.sh

--- SSH/dropbear/setup-dropbear.sh
+++ SSH/dropbear/setup-dropbear.sh
@@ -6,13 +6,13 @@ else
     stat="Already Instaled"
 fi
 
+default_port= "80"
+extra_port= "-p 144 -p 8080"
+
 echo "Status openSSH :$stat"
 apt install dropbear -y
 cp /etc/default/dropbear /etc/default/dropbear.bak
 wget -P /etc https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/tools/other/banner
-default_port= "80"
-extra_port= "-p 144 -p 8080"
-
 sed -i "s/NO_START=.*/NO_START=0/" /etc/default/dropbear
 sed -i "s/DROPBEAR_PORT=.*/DROPBEAR_PORT=80/" /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=.*/DROPBEAR_EXTRA_ARGS="-p 144 -p 8080"/g' /etc/default/dropbear

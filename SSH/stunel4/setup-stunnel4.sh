b44749e74c574eea478e1a81201233839c177675
lamtota40 <lamtota40@gmail.com>
2023-02-20 14:18:36 +0700

    Update setup-stunnel4.sh

--- SSH/stunel4/setup-stunnel4.sh
+++ SSH/stunel4/setup-stunnel4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt install -y stunnel4
-curl -skL "$GIST/stunnel.conf" -o /etc/stunnel/stunnel.conf
-curl -skL "$GIST/stunnel.pem" -o /etc/stunnel/stunnel.pem
+apt install stunnel4 -y
+curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/SSH/stunel4/stunnel.conf" -o /etc/stunnel/stunnel.conf
+curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/SSH/stunel4/stunnel.pem" -o /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 systemctl restart stunnel4
 if ! systemctl status stunnel4 &> /dev/null; then

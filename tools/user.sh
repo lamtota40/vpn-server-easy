11b4b9f74e033f9fe61f889216189f947ea4a8eb
lamtota40 <lamtota40@gmail.com>
2023-02-21 06:39:42 +0700

    Update user.sh

--- tools/user.sh
+++ tools/user.sh
@@ -4,6 +4,9 @@ echo "==================================="
 read -p "input username     : " username
 read -p "input password     : " password
 
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 useradd -p $pass -M username
 

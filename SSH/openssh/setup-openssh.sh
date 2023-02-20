92322b3e93cc72f5d9f027f36a0d315016cb9686
lamtota40 <lamtota40@gmail.com>
2023-02-21 00:53:22 +0700

    Update setup-openssh.sh

--- SSH/openssh/setup-openssh.sh
+++ SSH/openssh/setup-openssh.sh
@@ -1,1 +1,14 @@
-tes
+#!/bin/bash
+
+apt-get install openssh-server -y
+
+#Extra port openssh
+cd
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 2269" >> /etc/ssh/sshd_config
+sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+service ssh restart
+service sshd restart
+
+
+#apt-get purge openssh-server -y

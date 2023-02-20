ee0fc71cab3441c2f67376c96db20e299a8a0d14
lamtota40 <lamtota40@gmail.com>
2023-02-20 13:38:29 +0700

    Update setup-stunnel4.sh

--- SSH/stunel4/setup-stunnel4.sh
+++ SSH/stunel4/setup-stunnel4.sh
@@ -4,5 +4,5 @@ curl -skL "$GIST/stunnel.pem" -o /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 systemctl restart stunnel4
 if ! systemctl status stunnel4 &> /dev/null; then
-printf "\nFailed to install Stunnel4\n" && err
+   printf "\nFailed to install Stunnel4\n" && err
 fi

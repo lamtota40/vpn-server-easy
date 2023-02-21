3523280fcd8331583b985c27a1b7e2d120ed87d5
lamtota40 <lamtota40@gmail.com>
2023-02-21 14:15:12 +0700

    Update setup-dropbear.sh

--- SSH/dropbear/setup-dropbear.sh
+++ SSH/dropbear/setup-dropbear.sh
@@ -6,23 +6,24 @@ else
     stat="Already Instaled"
 fi
 
-default_port= "80"
-extra_port= "-p 144 -p 8080"
+#default_port= "80"
+#extra_port= "-p 144 -p 8080"
 
 echo "Status Dropbear :$stat"
-sleep 5
 apt install dropbear -y
 cp /etc/default/dropbear /etc/default/dropbear.bak
 sed -i "s/NO_START=.*/NO_START=0/" /etc/default/dropbear
 sed -i "s/DROPBEAR_PORT=.*/DROPBEAR_PORT=80/" /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=.*/DROPBEAR_EXTRA_ARGS="-p 144 -p 8080"/g' /etc/default/dropbear
 sed -i 's%DROPBEAR_BANNER=.*%DROPBEAR_BANNER="/etc/banner"%' /etc/default/dropbear
-
+sleep 20
 systemctl restart dropbear
 
 if ! systemctl status dropbear &> /dev/null; then
    printf "\nFailed to install Dropbear\n" && err
 fi
+
+rm -f setup-dropbear.sh
 
 #unistall
 #sudo apt-get purge dropbear -y

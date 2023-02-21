d3fee494630fd6424d1a98a9e27344a27584b7f1
lamtota40 <lamtota40@gmail.com>
2023-02-21 07:05:48 +0700

    Update user.sh

--- tools/user.sh
+++ tools/user.sh
@@ -8,6 +8,8 @@ echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-useradd -p $pass -M username
+#useradd -s /bin/false -M kolam
+#passwd kolam
+useradd -s /bin/false -p $pass -M username
 
 echo "selesai"

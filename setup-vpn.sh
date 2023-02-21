f1e59926aad1d64df1a34aece48108e9a4ce991e
lamtota40 <lamtota40@gmail.com>
2023-02-21 12:29:47 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -29,4 +29,4 @@ echo "/usr/sbin/nologin" >> /etc/shells
 
 #-1=MD5,-5=SHA256,-6=SHA512 (recommend)
 $cryptpass=openssl passwd -6 -salt xyz  $pass
-useradd -s /bin/false -p $cryptpass -M $Login
+useradd -s /bin/false -p $cryptpass -M $user

8f23a78b8cefac1dc6101258100548029b3e791b
lamtota40 <lamtota40@gmail.com>
2023-02-25 22:18:41 +0700

    Update monitoring.sh

--- tools/monitoring.sh
+++ tools/monitoring.sh
@@ -4,6 +4,7 @@ if [ ! $(which virt-what) ]; then
    apt install virt-what -y
 fi
 clear
+ram_use=$(free -h | grep Mem | awk '{print $3}')
 public_ip=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 private_ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n "$ip_number"p)
 cekip= $(wget https://proxycheck.io/v2/$public_ip?vpn=1&asn=1 -q -O -)
@@ -22,7 +23,7 @@ echo "Virtualization = " `if grep -Eoc '(vmx|svm)' /proc/cpuinfo; then echo "(en
 echo "Architecture = $(uname -m)"
 echo "OS = " `awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release`
 echo "CPU = "
-echo "RAM Usage = Mb | Free = Mb | Total = Mb"
+echo "RAM Usage = $ram_use | Free = Mb | Total = Mb"
 echo "HHD Usage = Mb | Free = Mb | Total = Mb"
 echo "=================================="
 echo "ok"

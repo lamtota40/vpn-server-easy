e1c3fab498acc6f038873d2491041794f1738afd
lamtota40 <lamtota40@gmail.com>
2023-02-26 09:01:32 +0700

    Update whologin.sh

--- tools/whologin.sh
+++ tools/whologin.sh
@@ -6,7 +6,7 @@ data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 echo "Checking User Dropbear logged in";
 echo "(PID - Username - IP)";
 echo "-----------------------------------------";
-x=0
+a=0
 for PID in "${data[@]}"
 do
 #echo "check $PID";
@@ -14,11 +14,11 @@ NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded
 USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
 IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
 if [ $NUM -eq 1 ]; then
-x=$((x+1))
+a=$((a+1))
 echo "$PID - $USER - $IP";
 fi
 done
-echo $x
+echo "Total User = $a"
  
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 echo "";

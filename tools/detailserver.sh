1fc3ae62a1910ad228a54ba9ff171d5211fc2e12
lamtota40 <lamtota40@gmail.com>
2023-02-26 19:05:53 +0700

    Update detailserver.sh

--- tools/detailserver.sh
+++ tools/detailserver.sh
@@ -31,7 +31,7 @@ echo "Virtualization = " `if grep -Eoc '(vmx|svm)' /proc/cpuinfo; then echo "(en
 echo "Architecture = $(uname -m)"
 echo "OS = $os_name $os_version"
 echo "Frimware = $([ -d /sys/firmware/efi ] && echo UEFI || echo BIOS)"
-echo "CPU(s) = $(lscpu | grep -oP "(?<=CPU(s):).*")"
+echo "CPU(s) = $(lscpu | grep -oP "(?<=CPU).*")"
 echo "Model name = $(lscpu | grep -oP "(?<=Model name:).*")"
 echo "Kernel = $(hostnamectl | grep -oP "(?<=Kernel:).*")"
 echo "RAM Free = $(awk '{print $4}' <<< "$raminfo") (${ram_free_p%.*} %) | Usage = $(awk '{print $3}' <<< "$raminfo") | Total = $(awk '{print $2}' <<< "$raminfo")"

42ecc556d9f6f3935a7c00293ce514ad0711c18b
lamtota40 <lamtota40@gmail.com>
2023-02-26 08:49:19 +0700

    Update whologin.sh

--- tools/whologin.sh
+++ tools/whologin.sh
@@ -9,12 +9,12 @@ echo "-----------------------------------------";
 x=0
 for PID in "${data[@]}"
 do
-x=$x+1
 #echo "check $PID";
 NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
 USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
 IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
 if [ $NUM -eq 1 ]; then
+x=$x++
 echo "$PID - $USER - $IP";
 fi
 done

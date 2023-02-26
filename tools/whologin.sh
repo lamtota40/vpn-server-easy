4d37a8c7dd25757ebb083c136eafb550c5775322
lamtota40 <lamtota40@gmail.com>
2023-02-26 08:33:32 +0700

    Update whologin.sh

--- tools/whologin.sh
+++ tools/whologin.sh
@@ -6,10 +6,10 @@ data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 echo "Memeriksa User Dropbear Yang Login";
 echo "(PID - Username - IP)";
 echo "-----------------------------------------";
-x=1
+x=0
 for PID in "${data[@]}"
 do
-x++
+x=x+1
 #echo "check $PID";
 NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
 USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;

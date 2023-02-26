c6d9f4204b742e09561b825e32221673ca27b330
lamtota40 <lamtota40@gmail.com>
2023-02-26 10:28:08 +0700

    Update detailserver.sh

--- tools/detailserver.sh
+++ tools/detailserver.sh
@@ -12,7 +12,7 @@ ram_free_p=$(free | grep Mem | awk '{print $4/$2 * 100}')
 ram_total=$(free -h | grep Mem | awk '{print $2}')
 public_ip=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 private_ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n "$ip_number"p)
-#cekip=$(wget https://proxycheck.io/v2/$public_ip?vpn=1&asn=1 -q -O -)
+cekip=$(curl -s "https://proxycheck.io/v2/$public_ip?vpn=1&asn=1")
 
 echo "========= Time on server ========="
 echo "Time =" `date "+%H:%M:%S"`

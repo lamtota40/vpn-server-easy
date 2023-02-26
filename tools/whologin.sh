968c0f8069b381f811c45bfe805fa7158b7a00bc
lamtota40 <lamtota40@gmail.com>
2023-02-26 09:03:43 +0700

    Update whologin.sh

--- tools/whologin.sh
+++ tools/whologin.sh
@@ -6,7 +6,7 @@ data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
 echo "Checking User Dropbear logged in";
 echo "(PID - Username - IP)";
 echo "-----------------------------------------";
-a=0
+a=b=0
 for PID in "${data[@]}"
 do
 #echo "check $PID";
@@ -18,7 +18,7 @@ a=$((a+1))
 echo "$PID - $USER - $IP";
 fi
 done
-echo "Total User = $a"
+echo "Total User Dropbear= $a"
  
 data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
 echo "";
@@ -32,7 +32,9 @@ NUM=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | gr
 USER=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $9}'`;
 IP=`cat /var/log/auth.log | grep -i sshd | grep -i "Accepted password for" | grep "sshd\[$PID\]" | awk '{print $11}'`;
 if [ $NUM -eq 1 ]; then
+b=$((b+1))
 echo "$PID - $USER - $IP";
 fi
 done
+echo "Total User OpenSSH= $b"
 echo "=========================================";

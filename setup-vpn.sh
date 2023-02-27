8d218071626238120504ded4a2553e8604f05976
lamtota40 <lamtota40@gmail.com>
2023-02-27 16:32:06 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -13,20 +13,23 @@ apt upgrade -y
 apt install curl openssl -y
 wget -P /etc $site/tools/other/banner
 
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+
 #install dropbear
 wget $site/SSH/dropbear/setup-dropbear.sh && bash setup-dropbear.sh
 
 #install stunnel4
 wget $site/SSH/stunel4/setup-stunnel4.sh && bash setup-stunnel4.sh
 
-#create account
-echo "==================================="
-read -p "input username     : " user
-read -p "input password     : " pass
-
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
-
 #-1=MD5,-5=SHA256,-6=SHA512 (recommend)
-cryptpass= $(openssl passwd -6 -salt xyz $pass)
-useradd -s /bin/false -p $cryptpass -M $user
+#cryptpass= $(openssl passwd -6 -salt xyz $pass)
+#useradd -s /bin/false -p $cryptpass -M $user
+
+apt install nginx -y
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://gitlab.com/hidessh/baru/-/raw/main/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://gitlab.com/hidessh/baru/-/raw/main/vps.conf"
+/etc/init.d/nginx restart

5693893d91f798fb4b8516bac77426e12013d1d0
lamtota40 <lamtota40@gmail.com>
2023-03-01 18:05:48 +0700

    Update setup-badvpn.sh

--- VPN/badvpn/setup-badvpn.sh
+++ VPN/badvpn/setup-badvpn.sh
@@ -4,11 +4,16 @@
 wget -O /usr/bin/badvpn-udpgw "https://github.com/lamtota40/vpn-server-easy/raw/main/VPN/badvpn/badvpn-udpgw64"
 chmod +x /usr/bin/badvpn-udpgw
 
+port1=7100
+port2=7200
+port3=7300
+
 for (( x=1; x<=3; x++ ))
 do 
-  cat > /etc/systemd/system/svr-7300.service <<-END
+xport="port$x"
+  cat > /etc/systemd/system/svr-$xport.service <<-END
 [Unit]
-Description=badvpn udpgw 7300
+Description=badvpn udpgw $xport
 After=network.target nss-lookup.target
 
 [Service]
@@ -18,17 +23,17 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Restart=on-failure
-ExecStart=/usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+ExecStart=/usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:$xport --max-clients 500
 
 [Install]
 WantedBy=multi-user.target
 END
-chmod +x /etc/systemd/system/svr-7300.service
+chmod +x /etc/systemd/system/svr-$xport.service
 
 systemctl daemon-reload
-systemctl start svr-7300.service
-systemctl enable svr-7300.service
-systemctl restart svr-7300.service
+systemctl start svr-$xport.service
+systemctl enable svr-$xport.service
+systemctl restart svr-$xport.service
 done
 
 rm -rf setup-dropbear.sh

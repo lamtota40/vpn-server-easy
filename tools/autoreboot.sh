38463e7dbec53c633de310065d2759e5a7d13658
lamtota40 <lamtota40@gmail.com>
2023-03-02 15:05:46 +0700

    Update autoreboot.sh

--- tools/autoreboot.sh
+++ tools/autoreboot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tanggal=$(date +"%m-%d-%Y")
-waktu=$(date +"%T")
-echo "Server sucessful reboot date $tanggal time $waktu." >> /root/log-reboot.txt
+s_date=$(date +"%m-%d-%Y")
+s_time=$(date +"%T")
+echo "Server sucessful reboot date $s_date time $s_time" >> /root/log-reboot.txt
 /sbin/shutdown -r now

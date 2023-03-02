1ac687c7ec0df919c27f9bc516b4878c8048855c
lamtota40 <lamtota40@gmail.com>
2023-03-02 19:09:33 +0700

    Update setup-dnstunnel.sh

--- dnstunnel/setup-dnstunnel.sh
+++ dnstunnel/setup-dnstunnel.sh
@@ -24,22 +24,15 @@ NS_DOMAIN=slowdns-${SUB_DOMAIN}
 echo $NS_DOMAIN > /root/nsdomain
 
 nameserver=$(cat /root/nsdomain)
-apt update -y
+
 apt install -y python3 python3-dnslib net-tools
 apt install ncurses-utils -y
 apt install dnsutils -y
-#apt install golang -y
 apt install git -y
-apt install curl -y
-apt install wget -y
 apt install ncurses-utils -y
 apt install screen -y
-apt install cron -y
-apt install iptables -y
-apt install -y git screen whois dropbear wget
-#apt install -y pwgen python php jq curl
-apt install -y sudo gnutls-bin
-#apt install -y mlocate dh-make libaudit-dev build-essential
+apt install whois -y
+apt install gnutls-bin -y
 apt install -y dos2unix debconf-utils
 service cron reload
 service cron restart

f5e7a01bdfce3951905e9e1d29f87c3d0b2bc89d
lamtota40 <lamtota40@gmail.com>
2023-03-03 11:18:51 +0700

    Update setup-squid.sh

--- PROXY/squid/setup-squid.sh
+++ PROXY/squid/setup-squid.sh
@@ -2,11 +2,11 @@
 
 PUBLIC_IP=$(wget -4qO- ipinfo.io/ip);
 
-apt install squid -y
-mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
-curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/squid/squid.conf" -o /etc/squid/squid.conf
-sed -i "s/xxxx/$PUBLIC_IP/g" /etc/squid/squid.conf
-systemctl restart squid
-if ! systemctl status squid &> /dev/null; then
+apt install squid3 -y
+mv /etc/squid3/squid.conf /etc/squid3/squid.conf.bak
+wget -P /etc/squid3/ "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/squid/squid.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf
+systemctl restart squid3
+if ! systemctl status squid3 &> /dev/null; then
     printf "\nFailed to install Squid\n" && err
 fi

d9f69bef4152b702f0fbe54bcc468be3f521f4f2
lamtota40 <lamtota40@gmail.com>
2023-03-03 18:45:45 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -36,21 +36,15 @@ chmod +x menu
 #install slowdns
 #wget $site/dnstunnel/setup-dnstunnel.sh && bash setup-dnstunnel.sh
 
+#installation nginx
+wget $site/nginx/nginx.conf && bash nginx.conf
+
 #auto reboot 24Hours
 wget -P /root $site/tools/autoreboot.sh
 chmod +x /root/autoreboot.sh
 echo "0 0 * * * root /root/autoreboot.sh" > /etc/cron.d/autoreboot
 service cron reload
 service cron restart
-
-#installation for nginx
-apt install nginx -y
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://gitlab.com/hidessh/baru/-/raw/main/nginx.conf"
-mkdir -p /home/vps/public_html
-wget -O /etc/nginx/conf.d/vps.conf "https://gitlab.com/hidessh/baru/-/raw/main/vps.conf"
-/etc/init.d/nginx restart
 
 Login="master"
 Pass="qwerty"

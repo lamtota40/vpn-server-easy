643f8c2b4a49b1cfb4f557a19fba64d978e8eec0
lamtota40 <lamtota40@gmail.com>
2023-03-03 13:39:00 +0700

    Update settings.sh

--- tools/settings.sh
+++ tools/settings.sh
@@ -6,3 +6,8 @@ echo "3. "Auto reboot"
 
 #change timezone
 timedatectl set-timezone Asia/Manila
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+

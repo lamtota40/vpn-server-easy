f69b3c3e003bcf0d0854429d904e23d2382e5c04
lamtota40 <lamtota40@gmail.com>
2023-03-12 20:08:59 +0700

    Update setup-squid.sh

--- PROXY/squid/setup-squid.sh
+++ PROXY/squid/setup-squid.sh
@@ -2,7 +2,7 @@
 
 PUBLIC_IP=$(wget -4qO- ipinfo.io/ip);
 
-apt install squid -y
+apt install squid apache2-utils -y
 mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
 wget -P /etc/squid/ "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/squid/squid.conf"
 sed -i $PUBLIC_IP /etc/squid/squid.conf

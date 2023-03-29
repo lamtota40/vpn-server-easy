52b0bba58741f7543bf9a44ce5f7c1feaa092eba
lamtota40 <lamtota40@gmail.com>
2023-03-29 21:46:27 +0700

    Update setup-ohp.sh

--- SSH/OHP/setup-ohp.sh
+++ SSH/OHP/setup-ohp.sh
@@ -3,3 +3,11 @@ unzip ohpserver-linux32.zip
 chmod +x ohpserver
 cp ohpserver /usr/local/bin/ohpserver
 /bin/rm -rf ohpserver*
+
+systemctl daemon-reload
+systemctl enable ssh-ohp
+systemctl restart ssh-ohp
+systemctl enable dropbear-ohp
+systemctl restart dropbear-ohp
+systemctl enable openvpn-ohp
+systemctl restart openvpn-ohp

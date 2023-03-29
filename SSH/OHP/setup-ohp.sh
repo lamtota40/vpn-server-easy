990df7d80fa564015b5175d27af0eac3fa4bc846
lamtota40 <lamtota40@gmail.com>
2023-03-29 19:44:51 +0700

    Update setup-ohp.sh

--- SSH/OHP/setup-ohp.sh
+++ SSH/OHP/setup-ohp.sh
@@ -1,1 +1,5 @@
-tes
+wget https://github.com/lfasmpao/open-http-puncher/releases/download/0.1/ohpserver-linux32.zip
+unzip ohpserver-linux32.zip
+chmod +x ohpserver
+cp ohpserver /usr/local/bin/ohpserver
+/bin/rm -rf ohpserver*

b8d2b4a7ec5793b6f8f564a5580d9615b1e833cb
lamtota40 <lamtota40@gmail.com>
2023-04-04 10:52:07 +0700

    Update create-ssl.sh

--- SSH/stunel4/create-ssl.sh
+++ SSH/stunel4/create-ssl.sh
@@ -13,11 +13,9 @@ echo "=================  membuat Sertifikat OpenSSL ======================"
 echo "========================================================="
 #membuat sertifikat
 openssl genrsa -out key.pem 2048
-#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
--subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$CN/emailAddress=$email"
+#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$CN/emailAddress=$email"
 
-openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
--subj "/CN=$CN"
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/CN=$CN"
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #rm -rf stunnel.sh

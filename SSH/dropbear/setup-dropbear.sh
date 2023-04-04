c5597c8a288f7a1a014071c61e6332b08b1b5d7f
lamtota40 <lamtota40@gmail.com>
2023-04-04 10:20:14 +0700

    Update setup-dropbear.sh

--- SSH/dropbear/setup-dropbear.sh
+++ SSH/dropbear/setup-dropbear.sh
@@ -14,7 +14,7 @@ apt install dropbear -y
 cp /etc/default/dropbear /etc/default/dropbear.bak
 sed -i "s/NO_START=.*/NO_START=0/" /etc/default/dropbear
 sed -i "s/DROPBEAR_PORT=.*/DROPBEAR_PORT=80/" /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=.*/DROPBEAR_EXTRA_ARGS="-p 144 -p 8080"/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=.*/DROPBEAR_EXTRA_ARGS="-p 23 -p 144 -p 7000"/g' /etc/default/dropbear
 sed -i 's%DROPBEAR_BANNER=.*%DROPBEAR_BANNER="/etc/banner"%' /etc/default/dropbear
 systemctl restart dropbear
 

46b626777e0780b4cfb965c0fc2af12a838b1aa7
lamtota40 <lamtota40@gmail.com>
2023-04-04 10:51:03 +0700

    Create create-ssl.sh

--- SSH/stunel4/create-ssl.sh
+++ SSH/stunel4/create-ssl.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # detail nama perusahaan
+CN=*.game.naver.com
+organization=NAVER
 country=ID
 state=Bandung
 locality=Jawa Barat
-organization=VPN Crops
 organizationalunit=myVPN
-CN=*.game.naver.com
 email=admin@vpncrops.com
 
 echo "=================  membuat Sertifikat OpenSSL ======================"
 echo "========================================================="
 #membuat sertifikat
 openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+#openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$CN/emailAddress=$email"
+
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/CN=$CN"
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #rm -rf stunnel.sh

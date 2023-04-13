b3f04021f0299e0ca570909333b9b03aadc9d623
lamtota40 <lamtota40@gmail.com>
2023-04-13 23:27:47 +0700

    Update coba.sh

--- coba/coba.sh
+++ coba/coba.sh
@@ -11,10 +11,28 @@ sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 69 -p 77 -p 300"/g' /etc/
 #echo "/bin/false" >> /etc/shells
 #echo "/usr/sbin/nologin" >> /etc/shells
 
-#instalasi Websocket
-#accept http port 80 to port 44
+#instalasi Websocket (accept http port 80 to port 44[dropbear])
 wget -O /usr/local/bin/ws-http https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/coba/ws-http.py && chmod +x /usr/local/bin/ws-http
 wget -O /etc/systemd/system/ws-http.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/ws-http.service
+cat > /etc/systemd/system/ws-http.service <<-END
+[Unit]
+Description=HTTP SSH Over Websocket Python HideSSH
+Documentation=https://hidessh.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+Restart=on-failure
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-http
+
+[Install]
+WantedBy=multi-user.target
+END
+chmod +x /etc/systemd/system/ws-http.service
 
 systemctl daemon-reload
 systemctl enable ws-http.service

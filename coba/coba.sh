cd546db356eca8fd6302b8b670cd25460a4c80de
lamtota40 <lamtota40@gmail.com>
2023-04-13 22:35:19 +0700

    Update coba.sh

--- coba/coba.sh
+++ coba/coba.sh
@@ -7,9 +7,9 @@ apt -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=44/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 69 -p 77 -p 300"/g' /etc/default/dropbear
-echo "/bin/false" >> /etc/shells
-echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
+#echo "/bin/false" >> /etc/shells
+#echo "/usr/sbin/nologin" >> /etc/shells
 
 #instalasi Websocket
 #accept http port 44 to port 88

c7e83bbcb41bfc74942cdbd8986de19700ceed57
lamtota40 <lamtota40@gmail.com>
2023-04-13 22:36:56 +0700

    Update coba.sh

--- coba/coba.sh
+++ coba/coba.sh
@@ -12,7 +12,7 @@ sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 69 -p 77 -p 300"/g' /etc/
 #echo "/usr/sbin/nologin" >> /etc/shells
 
 #instalasi Websocket
-#accept http port 44 to port 88
+#accept http port 80 to port 44
 wget -O /usr/local/bin/ws-http https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/coba/ws-http.py && chmod +x /usr/local/bin/ws-http
 wget -O /etc/systemd/system/ws-http.service https://gitlab.com/hidessh/baru/-/raw/main/websocket-python/baru/http.service && chmod +x /etc/systemd/system/ws-http.service
 

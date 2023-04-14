740ed3fe2ba25d4f3ea58fbaf69ba2fab364dfa7
lamtota40 <lamtota40@gmail.com>
2023-04-14 11:39:55 +0700

    Update setup-dropbear.sh

--- SSH/dropbear/setup-dropbear.sh
+++ SSH/dropbear/setup-dropbear.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ ! $(which dropbear) ]; then
-    stat="Not Instaled"
-else
-    stat="Already Instaled"
-fi
-
-#default_port= "80"
-#extra_port= "-p 144 -p 8080"
-
-echo "Status Dropbear :$stat"
 apt install dropbear -y
 cp /etc/default/dropbear /etc/default/dropbear.bak
 sed -i "s/NO_START=.*/NO_START=0/" /etc/default/dropbear

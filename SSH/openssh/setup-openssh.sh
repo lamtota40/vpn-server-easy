1792512319b4b5376a9dd098ee45e90f2925e097
lamtota40 <lamtota40@gmail.com>
2023-04-14 12:26:42 +0700

    Update setup-openssh.sh

--- SSH/openssh/setup-openssh.sh
+++ SSH/openssh/setup-openssh.sh
@@ -2,14 +2,18 @@
 
 apt-get install openssh-server -y
 
-#Extra port openssh
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
 echo "Port 143" >> /etc/ssh/sshd_config
 echo "Port 8000" >> /etc/ssh/sshd_config
-echo "Banner /etc/banner" >> /etc/ssh/sshd_config
-sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+sed -i 's/#Banner none/Banner /etc/banner/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+#sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+
 service ssh restart
 service sshd restart
+/etc/init.d/ssh restart
 
 
 #apt-get purge openssh-server -y

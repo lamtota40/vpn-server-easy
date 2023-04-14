6f788e019bf104cf54c76646060e6b524d56bb08
lamtota40 <lamtota40@gmail.com>
2023-04-14 22:34:20 +0700

    Update setup-sslh.sh

--- sslh/setup-sslh.sh
+++ sslh/setup-sslh.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 apt-get install sslh -y
-#konfigurasi
+
 #port 333 to 44 and 777
 wget -O /etc/default/sslh "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.conf"
 service sslh restart
+
+rm -rf setup-sslh.sh
+
+

5f55034ff4b3ed125beb79765d6dc15e6476c11d
lamtota40 <lamtota40@gmail.com>
2023-04-15 21:32:34 +0700

    Update setup-slowdns.sh

--- slowdns/setup-slowdns.sh
+++ slowdns/setup-slowdns.sh
@@ -39,9 +39,15 @@ service cron restart
 
 #tambahan port openssh
 cd
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
 echo "Port 2222" >> /etc/ssh/sshd_config
 echo "Port 2269" >> /etc/ssh/sshd_config
+sed -i 's%#Banner.*%Banner /etc/banner%' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+
 service ssh restart
 service sshd restart
 

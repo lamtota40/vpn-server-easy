df6cabec4d84916370e3c577973f4a0960ab61d9
lamtota40 <lamtota40@gmail.com>
2023-04-15 12:26:37 +0700

    Update setup-stunnel4.sh

--- SSH/stunel4/setup-stunnel4.sh
+++ SSH/stunel4/setup-stunnel4.sh
@@ -10,11 +10,15 @@ CN=*.whatsapp.com
 
 apt install stunnel4 -y
 
+rm -rf /etc/stunnel/stunnel.pem
+
+#create key.pem
 openssl genrsa -out key.pem 2048
+
+#create cert.pem
 #openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$CN/emailAddress=$email"
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/CN=$CN"
 
-rm -rf /etc/stunnel/stunnel.pem
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 
 #curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/SSH/stunel4/stunnel.conf" -o /etc/stunnel/stunnel.conf

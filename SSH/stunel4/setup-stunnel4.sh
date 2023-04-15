ea0b6e771b9d0009e475f45a0626f183438a94f2
lamtota40 <lamtota40@gmail.com>
2023-04-15 12:00:02 +0700

    Update setup-stunnel4.sh

--- SSH/stunel4/setup-stunnel4.sh
+++ SSH/stunel4/setup-stunnel4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # detail ssl
-CN=*.line.me
+CN=*.whatsapp.com
 organization=NAVER
 country=ID
 state=Bandung
@@ -9,14 +9,30 @@ organizationalunit=myVPN
 email=admin@vpncrops.com
 
 apt install stunnel4 -y
-curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/SSH/stunel4/stunnel.conf" -o /etc/stunnel/stunnel.conf
+
 openssl genrsa -out key.pem 2048
 #openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$CN/emailAddress=$email"
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095 -subj "/CN=$CN"
+
 rm -rf /etc/stunnel/stunnel.pem
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+#curl -skL "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/SSH/stunel4/stunnel.conf" -o /etc/stunnel/stunnel.conf
+
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/hidessh/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[dropbear]
+accept = 222
+connect = 127.0.0.1:22
+END
+
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-systemctl restart stunnel4
+/etc/init.d/stunnel4 restart
 
 if ! systemctl status stunnel4 &> /dev/null; then
    printf "\nFailed to install Stunnel4\n" && err

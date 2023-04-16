a1d293a14f20ad5a51d6f6d8d0011f68b69eeff6
lamtota40 <lamtota40@gmail.com>
2023-04-17 01:00:30 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -1,10 +1,8 @@
-sudo apt install dante-server
-sudo nano /etc/danted.conf
-netstat -plnt | grep -i danted
-sudo systemctl restart danted
-sudo systemctl enable danted
-sudo service danted status
-adduser --shell /usr/sbin/nologin --no-create-home proxyuser
-sudo passwd proxyuser
-curl http://portquiz.net --proxy "socks5://xxx.xxx.xxx.xxx:1080"
-curl http://portquiz.net --proxy "socks5://user:pass@xxx.xxx.xxx.xxx:1080"
+#!/bin/bash
+
+apt install dante-server -y
+wget -O /etc/danted.conf https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/shock/danted.conf
+systemctl restart danted
+systemctl enable danted
+
+rm -rf setup-dropbear.sh

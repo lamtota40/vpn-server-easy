6bd002173d4d4cfdf775cfcf7102e84b59ab736a
lamtota40 <lamtota40@gmail.com>
2023-04-16 22:02:16 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -1,1 +1,7 @@
-tes
+sudo apt install dante-server
+sudo nano /etc/danted.conf
+netstat -plnt | grep -i danted
+sudo service danted restart
+sudo service danted status
+adduser --shell /usr/sbin/nologin --no-create-home danted-user
+curl -x "socks5://username:password@ip-server:port" -skL "https://ipinfo.io"

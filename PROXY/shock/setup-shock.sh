febfdfd83074d2cbc449f1735d4020365bc78b8f
lamtota40 <lamtota40@gmail.com>
2023-04-16 22:27:58 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -6,6 +6,4 @@ sudo systemctl enable danted
 sudo service danted status
 adduser --shell /usr/sbin/nologin --no-create-home proxyuser
 sudo passwd proxyuser
-curl -x "socks5://username:password@ip-server:port" -skL "https://ipinfo.io"
-
-curl -x "socks5://3.1.58.120:1080" -skL "https://ipinfo.io"
+curl https://ipinfo.io -x "socks5://master:qwerty@3.1.58.120:1080"

bbd8c9894d09ad0fedf3554ba64c4e23cbcab494
lamtota40 <lamtota40@gmail.com>
2023-04-16 22:23:04 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -1,7 +1,11 @@
 sudo apt install dante-server
 sudo nano /etc/danted.conf
 netstat -plnt | grep -i danted
-sudo service danted restart
+sudo systemctl restart danted
+sudo systemctl enable danted
 sudo service danted status
-adduser --shell /usr/sbin/nologin --no-create-home danted-user
+adduser --shell /usr/sbin/nologin --no-create-home proxyuser
+sudo passwd proxyuser
 curl -x "socks5://username:password@ip-server:port" -skL "https://ipinfo.io"
+
+curl -x "socks5://3.1.58.120:1080" -skL "https://ipinfo.io"

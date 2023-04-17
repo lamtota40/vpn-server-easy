09b58758c14c90248608f3de8f8629071ca9de43
lamtota40 <lamtota40@gmail.com>
2023-04-17 08:39:47 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Dante is proxy server shock4 & shock5
-#for testing: curl -v https://ipinfo.io --proxy "socks4://3.1.58.120:1080"
+#for testing: curl -v https://ipinfo.io --proxy "socks4://user:pass@ip:1080"
 apt install dante-server -y
 wget -O /etc/danted.conf https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/shock/danted.conf
 systemctl restart danted

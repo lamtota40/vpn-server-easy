6b323cfe086a0436401ab70c1b437fd0643e155b
lamtota40 <lamtota40@gmail.com>
2023-04-17 09:02:45 +0700

    Update setup-shock.sh

--- PROXY/shock/setup-shock.sh
+++ PROXY/shock/setup-shock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Dante is proxy server shock4 & shock5
-#shock4 not support "username" authenticated
+#The authentication method "username" is not supported by version 4 of the SOCKS protocol, only by SOCKS v5
 #for testing: curl -v https://ipinfo.io --proxy "socks4://user:pass@ip:1080"
 apt install dante-server -y
 wget -O /etc/danted.conf https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/PROXY/shock/danted.conf

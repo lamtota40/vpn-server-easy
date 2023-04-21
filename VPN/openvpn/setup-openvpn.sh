29b8257fb465f9863a5b283ab6677ca31a929719
lamtota40 <lamtota40@gmail.com>
2023-04-21 16:48:20 +0700

    Update setup-openvpn.sh

--- VPN/openvpn/setup-openvpn.sh
+++ VPN/openvpn/setup-openvpn.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
+
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-# By Lakmal Sandaru
-# ==================================================
-# Link Hosting Kalian
 lakmalvpn="raw.githubusercontent.com/godtrex99/V2vps/main/ssh"
 
 # initialisasi var
@@ -151,5 +136,4 @@ systemctl start openvpn
 /etc/init.d/openvpn restart
 
 # Delete script
-history -c
-rm -f /root/vpn.sh
+#history -c

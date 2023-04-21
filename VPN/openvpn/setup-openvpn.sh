ad190afbe23083c568d5344795e6e950b21accad
lamtota40 <lamtota40@gmail.com>
2023-04-21 16:59:17 +0700

    Update setup-openvpn.sh

--- VPN/openvpn/setup-openvpn.sh
+++ VPN/openvpn/setup-openvpn.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 MYIP=$(wget -qO- ipinfo.io/ip);
-lakmalvpn="raw.githubusercontent.com/godtrex99/V2vps/main/ssh"
 
-# initialisasi var
+# initialion var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 MYIP=$(wget -qO- ipinfo.io/ip);
@@ -15,7 +14,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://${lakmalvpn}/vpn.zip
+wget https://github.com/lamtota40/vpn-server-easy/raw/main/VPN/openvpn/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
@@ -27,17 +26,16 @@ cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib
 # nano /etc/default/openvpn
 sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
 
-# restart openvpn dan cek status openvpn
+# restart openvpn
 systemctl enable --now openvpn-server@server-tcp
 systemctl enable --now openvpn-server@server-udp
 /etc/init.d/openvpn restart
-/etc/init.d/openvpn status
 
-# aktifkan ip4 forwarding
+# enable ip4 forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
-# Buat config client TCP 1194
+# Create config client TCP 1194
 cat > /etc/openvpn/tcp.ovpn <<-END
 client
 dev tun
@@ -55,7 +53,7 @@ END
 
 sed -i $MYIP2 /etc/openvpn/tcp.ovpn;
 
-# Buat config client UDP 2200
+# Create config client UDP 2200
 cat > /etc/openvpn/udp.ovpn <<-END
 client
 dev tun
@@ -73,7 +71,7 @@ END
 
 sed -i $MYIP2 /etc/openvpn/udp.ovpn;
 
-# Buat config client SSL
+# Create config client SSL
 cat > /etc/openvpn/ssl.ovpn <<-END
 client
 dev tun

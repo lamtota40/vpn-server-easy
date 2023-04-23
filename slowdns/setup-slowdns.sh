d9e17e5a6ccdd4bbd23b334165589aa10cfe8e68
lamtota40 <lamtota40@gmail.com>
2023-04-23 12:32:05 +0700

    Update setup-slowdns.sh

--- slowdns/setup-slowdns.sh
+++ slowdns/setup-slowdns.sh
@@ -4,6 +4,7 @@
 apt install iptables netfilter-persistent -y
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
 iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
 netfilter-persistent reload

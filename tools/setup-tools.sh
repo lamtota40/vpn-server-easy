4a8a35fcfbddc4989716b0b1b875e39729bcdc8e
lamtota40 <lamtota40@gmail.com>
2023-04-26 20:08:54 +0700

    Update setup-tools.sh

--- tools/setup-tools.sh
+++ tools/setup-tools.sh
@@ -3,5 +3,5 @@
 site="https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main"
 wget -O myvpn/adduser $site/tools/adduser.sh && chmod +x adduser
 wget -O myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x speedtest
-
+wget -O myvpn/ram $site/tools/ram.sh && chmod +x ram
 rm -rf setup-tools.sh

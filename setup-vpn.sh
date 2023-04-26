71cda24abc71c562cad67fb08abccc8bdd4f9472
lamtota40 <lamtota40@gmail.com>
2023-04-26 19:47:49 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -73,9 +73,8 @@ wget $site/VPN/openvpn/setup-openvpn.sh && bash setup-openvpn.sh
 #wget https://raw.githubusercontent.com/godtrex99/V2vps/1c0d89c80a81661cca63eed089f0b72492b2fddc/ssh/vpn.sh && bash vpn.sh
 
 #download menu & tools
-cd
-mkdir -p myvpn
 wget $site/tools/setup-tools.sh && bash setup-tools.sh
+cd
 wget -O menu $site/tools/menu && chmod +x menu
 
 

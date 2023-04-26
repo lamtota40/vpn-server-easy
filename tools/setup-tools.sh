3fb48e65851638719a78b2db9caffe770cd7bd2c
lamtota40 <lamtota40@gmail.com>
2023-04-27 00:32:24 +0700

    Update setup-tools.sh

--- tools/setup-tools.sh
+++ tools/setup-tools.sh
@@ -2,9 +2,9 @@
 
 mkdir myvpn/cron
 site="https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main"
-wget -O myvpn/listusers.sh $site/tools/listusers.sh && chmod +x listusers.sh
-wget -O myvpn/adduser $site/tools/adduser.sh && chmod +x adduser
-wget -O myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x speedtest
-wget -O myvpn/ram $site/tools/ram.sh && chmod +x ram
-
+wget -O /root/myvpn/listusers.sh $site/tools/listusers.sh && chmod +x listusers.sh
+wget -O /root/myvpn/adduser $site/tools/adduser.sh && chmod +x adduser
+wget -O /root/myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x speedtest
+wget -O /root/myvpn/ram $site/tools/ram.sh && chmod +x ram
+wget -O /root/myvpn/detailserver.h $site/tools/detailserver.sh && chmod +x /root/myvpn/detailserver.sh
 rm -rf setup-tools.sh

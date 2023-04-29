b0b7095676825e1af8f268ae0c231dcf34b2c934
lamtota40 <lamtota40@gmail.com>
2023-04-29 17:54:55 +0700

    Update setup-nginx.sh

--- nginx/setup-nginx.sh
+++ nginx/setup-nginx.sh
@@ -4,7 +4,7 @@ apt install nginx -y
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/nginx/nginx.conf"
-mkdir -p /home/vps/public_html
+mkdir -p /var/myvpn/public_html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/nginx/vps.conf"
 /etc/init.d/nginx restart
 

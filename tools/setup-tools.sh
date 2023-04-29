8ec83d1604d4609f36e5c7dac82acfb1bac212c4
lamtota40 <lamtota40@gmail.com>
2023-04-29 23:37:53 +0700

    Update setup-tools.sh

--- tools/setup-tools.sh
+++ tools/setup-tools.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-mkdir myvpn/cron
 site="https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main"
-wget -O /root/myvpn/listusers.sh $site/tools/listusers.sh && chmod +x listusers.sh
-wget -O /root/myvpn/adduser $site/tools/adduser.sh && chmod +x adduser
-wget -O /root/myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x speedtest
-wget -O /root/myvpn/ram $site/tools/ram.sh && chmod +x ram
-wget -O /root/myvpn/detailserver.h $site/tools/detailserver.sh && chmod +x /root/myvpn/detailserver.sh
+wget -O /root/myvpn/listusers.sh $site/tools/listusers.sh && chmod +x /root/myvpn/listusers.sh
+wget -O /root/myvpn/adduser $site/tools/adduser.sh && chmod +x /root/myvpn/adduser
+wget -O /root/myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x /root/myvpn/speedtest
+wget -O /root/myvpn/ram $site/tools/ram.sh && chmod +x /root/myvpn/ram
+wget -O /root/myvpn/detailserver.sh $site/tools/detailserver.sh && chmod +x /root/myvpn/detailserver.sh
+wget -O /root/myvpn/whologin.sh $site/tools/whologin.sh && chmod +x /root/myvpn/whologin.sh
+
 rm -rf setup-tools.sh

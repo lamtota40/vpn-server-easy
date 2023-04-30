af04f32d344c1eb9bf6b240f103947ac25ef64d0
lamtota40 <lamtota40@gmail.com>
2023-04-30 18:11:41 +0700

    Update setup-websocket.sh

--- websocket/setup-websocket.sh
+++ websocket/setup-websocket.sh
@@ -26,10 +26,33 @@ ExecStart=/usr/bin/python -O /usr/local/bin/ws-http80
 WantedBy=multi-user.target
 END
 chmod +x /etc/systemd/system/ws-http80.service
+###################################################
+#instalasi Websocket (accept http port 2082 forward to port 23[dropbear])
+wget -O /usr/local/bin/ws-http2082 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-http2082.py && chmod +x /usr/local/bin/ws-http2082
 
+cat > /etc/systemd/system/ws-http2082.service <<-END
+[Unit]
+Description=HTTP SSH Over Websocket 2082 Python
+Documentation=https://github.com/lamtota40
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+Restart=on-failure
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-http2082
+
+[Install]
+WantedBy=multi-user.target
+END
+chmod +x /etc/systemd/system/ws-http2082.service
+###################################################
 #instalasi Websocket (accept http port 8880 forward to port 23[dropbear])
 wget -O /usr/local/bin/ws-http8880 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-http8880.py && chmod +x /usr/local/bin/ws-http8880
-#########################################
+
 cat > /etc/systemd/system/ws-http8880.service <<-END
 [Unit]
 Description=HTTP SSH Over Websocket 8880 Python
@@ -49,11 +72,15 @@ ExecStart=/usr/bin/python -O /usr/local/bin/ws-http8880
 WantedBy=multi-user.target
 END
 chmod +x /etc/systemd/system/ws-http8880.service
-
+##################################################
 systemctl daemon-reload
 systemctl enable ws-http80.service
 systemctl start ws-http80.service
 systemctl restart ws-http80.service
+
+systemctl enable ws-http2082.service
+systemctl start ws-http2082.service
+systemctl restart ws-http2082.service
 
 systemctl enable ws-http8880.service
 systemctl start ws-http8880.service

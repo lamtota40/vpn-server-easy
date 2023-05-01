fbbaad72db31419b78191a900f128e89ead69258
lamtota40 <lamtota40@gmail.com>
2023-05-02 06:47:27 +0700

    Update menu.sh

--- tools/menu.sh
+++ tools/menu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PUBLIC_IP=$(wget -4qO- ipinfo.io/ip);
-raminfo=$(free -h | grep Mem)
-ram_free_p=$(free | grep Mem | awk '{print $4/$2 * 100}')
+raminfo=$(free -h | grep Mem)=$(free | grep Mem | awk '{print $4/$2 * 100}')
+swap_free_p=$(free -h | grep Mem)=$(free | grep Mem | awk '{print $4/$2 * 100}')
 cekip=$(curl -s "https://get.geojs.io/v1/ip/geo.json")
 domain="zerostore.sit.my.id"
 
@@ -13,8 +13,8 @@ echo "Uptime =" `uptime -p`
 echo "Domain : $domain"
 echo "Ip Public = $PUBLIC_IP"
 echo "ISP = $(jq -r '.organization_name' <<< "$cekip") | Country= $(jq -r '.country' <<< "$cekip")"
-echo "RAM | Free= $(awk '{print $4}' <<< "$raminfo") (${ram_free_p%.*} %) | Usage = $(awk '{print $3}' <<< "$raminfo") | Total = $(awk '{print $2}' <<< "$raminfo")"
-echo "SWAP RAM | Free = $(awk 'FNR == 2 {print$3-$4}' <<< "$swapinfo" | numfmt --to=iec) (${ram_free_p%.*} %) | Usage = $(awk 'FNR == 2 {print$4}' <<< "$swapinfo" | numfmt --to=iec) | Total = $(awk 'FNR == 2 {print$3}' <<< "$swapinfo" | numfmt --to=iec)"
+echo "RAM [Free= $(awk '{print $4}' <<< "$raminfo") (${ram_free_p%.*} %) | Usage = $(awk '{print $3}' <<< "$raminfo") | Total = $(awk '{print $2}' <<< "$raminfo")]"
+echo "SWAP RAM [Free = $(awk 'FNR == 2 {print$3-$4}' <<< "$swapinfo" | numfmt --to=iec) (${ram_free_p%.*} %) | Usage = $(awk 'FNR == 2 {print$4}' <<< "$swapinfo" | numfmt --to=iec) | Total = $(awk 'FNR == 2 {print$3}' <<< "$swapinfo" | numfmt --to=iec)"
 echo "================================"
 echo " ++++++++++++ Menu ++++++++++++"
 echo "================================"

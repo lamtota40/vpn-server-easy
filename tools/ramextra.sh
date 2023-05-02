43fe59d414dfcec6b6c13bb2923cada16d746efc
lamtota40 <lamtota40@gmail.com>
2023-05-02 18:51:09 +0700

    Update ramextra.sh

--- tools/ramextra.sh
+++ tools/ramextra.sh
@@ -1,4 +1,6 @@
-sudo fallocate -l 2 /swapfile
+#!/bin/bash
+
+sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
@@ -8,3 +10,5 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 sudo bash -c "echo -e 'vm.swappiness=10\nvm.vfs_cache_pressure=50' >> /etc/sysctl.conf"
 sudo sysctl vm.swappiness=10
 sudo sysctl vm.vfs_cache_pressure=50
+
+rm -rf setup-vpn.sh

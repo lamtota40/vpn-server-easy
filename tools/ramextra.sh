37a7a3ad562239098900ed758d08e509d1b2e7f6
lamtota40 <lamtota40@gmail.com>
2023-05-02 18:45:42 +0700

    Update and rename extraram.sh to ramextra.sh

---
+++ tools/ramextra.sh
@@ -0,0 +1,10 @@
+sudo fallocate -l 2 /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo bash -c "echo -e 'vm.swappiness=10\nvm.vfs_cache_pressure=50' >> /etc/sysctl.conf"
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50

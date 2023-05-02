ddcaf4cb4a74cc2bef5cab8c6dc1ddb805df34e7
lamtota40 <lamtota40@gmail.com>
2023-05-02 16:47:14 +0700

    Update addudp.sh

--- tools/addudp.sh
+++ tools/addudp.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+clear
 
 PUBLIC_IP=$(wget -4qO- ipinfo.io/ip);
-clear
+domain="zerostore.sit.my.id"
+$username="udp"
 echo "======================================"
 echo "ADD account for UDP"
 echo "Note: For cancel use CTRL+C"
 echo "======================================"
-echo "Username         : udp "
+echo "Username         : $username "
 read -p "input Password     : " pass
 read -p "Expired (day)      : " exp
 
-expdate="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+pasword=$pass
+#expdate="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 clear
 echo -e"◇━━━━━━━━━━━━━━━━━◇"
 echo -e"(UDP Account)-Zero Store"
 echo -e"https://t.me/zerovpn3"
 echo -e"◇━━━━━━━━━━━━━━━━━◇"
-echo -e "Username   : udp "
-echo -e "Password   : $pass"
-echo -e "Expired    : $expdate"
-echo -e "Domain     : "
-echo -e "IP         : $PUBLIC_IP"
-echo -e "Port       : 1-65535"
+echo -e "Username : udp "
+echo -e "Pasword  : $pasword"
+echo -e "Expired  : $exp"
+echo -e "Domain   : $domain"
+echo -e "IP       : $PUBLIC_IP"
+echo -e "Port     : 1-65535"
 echo -e"◇━━━━━━━━━━━━━━━━━◇"                               

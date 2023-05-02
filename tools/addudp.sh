b8cdce5b16fa09a31701f03718b249614b5e5110
lamtota40 <lamtota40@gmail.com>
2023-05-02 16:41:27 +0700

    Update addudp.sh

--- tools/addudp.sh
+++ tools/addudp.sh
@@ -13,14 +13,13 @@ read -p "Expired (day)      : " exp
 expdate="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 clear
 echo -e"◇━━━━━━━━━━━━━━━━━◇"
-echo -e".   ⟨ UDP Account ⟩"
+echo -e"(UDP Account)-Zero Store"
+echo -e" https://t.me/zerovpn3"
 echo -e"◇━━━━━━━━━━━━━━━━━◇"
-echo -e "==============================="
 echo -e "Username   : udp "
 echo -e "Password   : $pass"
 echo -e "Expired    : $expdate"
 echo -e "Domain     : "
 echo -e "IP         : $PUBLIC_IP"
 echo -e "Port       : 1-65535"
-echo -e "==============================="
-                                 
+echo -e"◇━━━━━━━━━━━━━━━━━◇"                               

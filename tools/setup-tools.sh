6c26d77777fd72e8ead297df566b9a6f4a660556
lamtota40 <lamtota40@gmail.com>
2023-05-04 05:51:12 +0700

    Update setup-tools.sh

--- tools/setup-tools.sh
+++ tools/setup-tools.sh
@@ -7,6 +7,6 @@ wget -O /root/myvpn/listusers.sh $site/tools/listusers.sh && chmod +x /root/myvp
 wget -O /root/myvpn/speedtest $site/tools/speedtest_cli.py && chmod +x /root/myvpn/speedtest
 wget -O /root/myvpn/ram $site/tools/ram.sh && chmod +x /root/myvpn/ram
 wget -O /root/myvpn/detailserver.sh $site/tools/detailserver.sh && chmod +x /root/myvpn/detailserver.sh
-wget -O /root/myvpn/whologin.sh $site/tools/whologin.sh && chmod +x /root/myvpn/whologin.sh
+wget -O /root/myvpn/whologin $site/tools/whologin.sh && chmod +x /root/myvpn/whologin
 
 rm -rf setup-tools.sh

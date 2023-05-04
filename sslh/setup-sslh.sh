81f76cea90230b74454d383ef13191157783e6fb
lamtota40 <lamtota40@gmail.com>
2023-05-04 10:05:36 +0700

    Update setup-sslh.sh

--- sslh/setup-sslh.sh
+++ sslh/setup-sslh.sh
@@ -3,7 +3,7 @@
 #https://github.com/yrutschle/sslh
 #SSLH support TCP UDP & Probes for HTTP, TLS/SSL (including SNI and ALPN), SSH, OpenVPN, tinc, XMPP, SOCKS5,
 
-apt-get install sslh --no-install-recommends -y
+DEBIAN_FRONTEND=noninteractive apt-get install sslh --no-install-recommends -y
 #port 443,5222,5228
 wget -O /etc/default/sslh "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.conf"
 service sslh restart

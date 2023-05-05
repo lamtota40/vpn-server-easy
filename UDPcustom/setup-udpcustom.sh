6b29743618fb03a1ece9501a3eff3bc82f4d7cec
lamtota40 <lamtota40@gmail.com>
2023-05-05 14:26:53 +0700

    Update setup-udpcustom.sh

--- UDPcustom/setup-udpcustom.sh
+++ UDPcustom/setup-udpcustom.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 cd
-rm -rf /root/udp
-mkdir -p /root/udp
+rm -rf /root/myvpn/udp
+mkdir -p /root/myvpn/udp
 
 # change to time GMT+7
 #ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # install udp-custom
-wget -O /root/udp/udp-custom "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/UDPcustom/udp-custom"
-chmod +x /root/udp/udp-custom
+wget -O /root/myvpn/udp/udp-custom "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/UDPcustom/udp-custom"
+chmod +x /root/myvpn/udp/udp-custom
 
 #download config
-wget -O /root/udp/config.json "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/UDPcustom/config.json"
-chmod 644 /root/udp/config.json
+wget -O /root/myvpn/udp/config.json "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/UDPcustom/config.json"
+chmod 644 /root/myvpn/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
@@ -23,8 +23,8 @@ Description=UDP Custom by ePro Dev. Team
 [Service]
 User=root
 Type=simple
-ExecStart=/root/udp/udp-custom server
-WorkingDirectory=/root/udp/
+ExecStart=/root/myvpn/udp/udp-custom server
+WorkingDirectory=/root/myvpn/udp/
 Restart=always
 RestartSec=2s
 
@@ -39,8 +39,8 @@ Description=UDP Custom by ePro Dev. Team
 [Service]
 User=root
 Type=simple
-ExecStart=/root/udp/udp-custom server -exclude $1
-WorkingDirectory=/root/udp/
+ExecStart=/root/myvpn/udp/dp-custom server -exclude $1
+WorkingDirectory=/root/myvpn/udp/
 Restart=always
 RestartSec=2s
 
@@ -49,7 +49,7 @@ WantedBy=default.target
 EOF
 fi
 
-systemctl start udp-custom &>/dev/null
 systemctl enable udp-custom &>/dev/null
-
+systemctl start udp-custom &>/dev/null
+systemctl restart udp-custom &>/dev/null
 

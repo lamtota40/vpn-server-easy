0ce5e01705db08eea50778e3ceaca832be4f5ad1
lamtota40 <lamtota40@gmail.com>
2023-05-08 16:54:34 +0700

    Update setup-slowdns.sh

--- slowdns/setup-slowdns.sh
+++ slowdns/setup-slowdns.sh
@@ -70,7 +70,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:8000
+ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
 Restart=on-failure
 
 [Install]

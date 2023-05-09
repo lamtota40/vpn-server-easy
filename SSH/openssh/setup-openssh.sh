bc8f639e5fb3f432fcce305e80630fee29d7c559
lamtota40 <lamtota40@gmail.com>
2023-05-09 11:10:06 +0700

    Update setup-openssh.sh

--- SSH/openssh/setup-openssh.sh
+++ SSH/openssh/setup-openssh.sh
@@ -10,7 +10,7 @@ echo "Port 143" >> /etc/ssh/sshd_config
 echo "Port 8000" >> /etc/ssh/sshd_config
 sed -i 's%#Banner.*%Banner /etc/banner%' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication .*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication .*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication .*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
 service sshd restart
 #apt-get purge openssh-server -y
 

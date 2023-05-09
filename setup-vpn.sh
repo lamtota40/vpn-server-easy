6529571a811bc2761c12f5e717792f5399407b07
lamtota40 <lamtota40@gmail.com>
2023-05-09 10:51:20 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -12,10 +12,10 @@ mkdir -p /root/myvpn/data
 mkdir -p /root/myvpn/config
 mkdir -p /root/myvpn/cron
 
-read -p "input your domain" domain
+read -p "input your domain = " domain
 echo $domain > /root/myvpn/domain
 #chmod +x /root/myvpn/domain
-read -p "input your NS Domain" domain
+read -p "input your NS Domain =" domain
 echo $domain > /root/myvpn/nsdomain
 #chmod +x /root/myvpn/nsdomain
 

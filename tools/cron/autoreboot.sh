808c6426faeaac2adc2f96a02239158afe0a36ab
lamtota40 <lamtota40@gmail.com>
2023-05-12 12:41:29 +0700

    Update autoreboot.sh

--- tools/cron/autoreboot.sh
+++ tools/cron/autoreboot.sh
@@ -2,5 +2,5 @@
 
 s_date=$(date +"%m-%d-%Y")
 s_time=$(date +"%T")
-echo "Server sucessful reboot date $s_date time $s_time" >> /root/myvpn/cron/log-cron.txt
+echo "Server sucessful reboot date $s_date time $s_time" >> /root/myvpn/cron/logcron.txt
 /sbin/shutdown -r now

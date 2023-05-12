213959a61395b895b96888c2d9987521be3edd49
lamtota40 <lamtota40@gmail.com>
2023-05-12 12:43:28 +0700

    Update autodelexp.sh

--- tools/cron/autodelexp.sh
+++ tools/cron/autodelexp.sh
@@ -3,4 +3,4 @@
 s_date=$(date +"%m-%d-%Y")
 s_time=$(date +"%T")
 echo "Server sucessful reboot date $s_date time $s_time" >> /root/myvpn/cron/logcron.txt
-/sbin/shutdown -r now
+userdel -f -r $username

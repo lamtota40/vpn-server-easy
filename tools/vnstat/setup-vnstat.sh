61fa62ad2a9f3f2bc40f4daacaed8ef1263d0ab4
lamtota40 <lamtota40@gmail.com>
2023-05-13 07:30:26 +0700

    Update setup-vnstat.sh

--- tools/vnstat/setup-vnstat.sh
+++ tools/vnstat/setup-vnstat.sh
@@ -4,9 +4,9 @@
 apt install vnstat make gcc -y
 /etc/init.d/vnstat restart
 apt -y install libsqlite3-dev
-wget -O vnstat-2.6.tar.gz https://humdi.net/vnstat/vnstat-2.6.tar.gz
-tar zxvf vnstat-2.6.tar.gz
-cd vnstat-2.6
+wget -O vnstat-2.10.tar.gz https://humdi.net/vnstat/vnstat-2.10.tar.gz
+tar zxvf vnstat-2.10.tar.gz
+cd vnstat-2.10
 ./configure --prefix=/usr --sysconfdir=/etc && make && make install
 cd
 vnstat -u -i $NET
@@ -15,7 +15,7 @@ chown vnstat:vnstat /var/lib/vnstat -R
 systemctl enable vnstat
 etc/init.d/vnstat restart
 
-rm -f vnstat-2.6.tar.gz
-rm -rf vnstat-2.6
+rm -f vnstat-2.10.tar.gz
+rm -rf vnstat-2.10
 
 rm -rf setup-vnstat.sh

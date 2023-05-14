53221a1c4e6b3d9f31360376b3dbdb0ac49ffc13
lamtota40 <lamtota40@gmail.com>
2023-05-14 09:54:34 +0700

    Update setup-sslh.sh

--- sslh/setup-sslh.sh
+++ sslh/setup-sslh.sh
@@ -8,6 +8,18 @@ DEBIAN_FRONTEND=noninteractive apt-get install sslh --no-install-recommends -y
 wget -O /etc/default/sslh "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.conf"
 service sslh restart
 
+[Unit]
+Description=SSL/SSH multiplexer
+After=network.target
+
+[Service]
+EnvironmentFile=/etc/conf.d/sslh
+ExecStart=/usr/bin/sslh --foreground $DAEMON_OPTS
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+
 rm -rf setup-sslh.sh
 
 

a4a06ddca3b876683c59a10a1575ecc9bd1fcdcd
lamtota40 <lamtota40@gmail.com>
2023-05-14 14:54:10 +0700

    Update setup-sslh.sh

--- sslh/setup-sslh.sh
+++ sslh/setup-sslh.sh
@@ -7,13 +7,13 @@ DEBIAN_FRONTEND=noninteractive apt-get install sslh --no-install-recommends -y
 wget -O /etc/default/sslh "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.conf"
 service sslh restart
 
-cat <<EOF > /etc/systemd/system/sslh80.service
+cat <<EOF > /etc/systemd/system/sslh443.service
 [Unit]
 Description=SSL/SSH multiplexer
 After=network.target
 
 [Service]
-ExecStart=/usr/sbin/sslh --foreground --user sslh --listen 0.0.0.0:80 --ssh 127.0.0.1:23 --tls 127.0.0.1:955 --openvpn 127.0.0.1:111 --http 127.0.0.1:2082 --pidfile /var/run/sslh/sslh80.pid
+ExecStart=/usr/sbin/sslh --foreground --user sslh --listen 0.0.0.0:443 --ssh 127.0.0.1:23 --tls 127.0.0.1:955 --openvpn 127.0.0.1:111 --http 127.0.0.1:2082 --pidfile /var/run/sslh/sslh443.pid
 KillMode=process
 
 [Install]
@@ -21,9 +21,7 @@ WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl enable sslh80
-systemctl start sslh80
+systemctl enable sslh443
+systemctl start sslh443
 
 rm -rf setup-sslh.sh
-
-

25a22f2a044334335f1c687a3eb19ec3d866ca54
lamtota40 <lamtota40@gmail.com>
2023-05-16 10:03:17 +0700

    Update setup-sslh.sh

--- sslh/setup-sslh.sh
+++ sslh/setup-sslh.sh
@@ -23,5 +23,22 @@ EOF
 systemctl daemon-reload
 systemctl enable sslh443
 systemctl start sslh443
+##########################
+cat <<EOF > /etc/systemd/system/sslh77.service
+[Unit]
+Description=SSL/SSH multiplexer
+After=network.target
+
+[Service]
+ExecStart=/usr/sbin/sslh --foreground --user sslh --listen 0.0.0.0:77 --ssh 127.0.0.1:22 --tls 127.0.0.1:955 --openvpn 127.0.0.1:111 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh77.pid
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable sslh77
+systemctl start sslh77
 
 rm -rf setup-sslh.sh

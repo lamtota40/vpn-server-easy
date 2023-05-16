c19e63bfde1b201ad657fda0f9c4932ed66a7965
lamtota40 <lamtota40@gmail.com>
2023-05-16 08:45:00 +0700

    Create iptables.sh

---
+++ tools/iptables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SlowDNS
+iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j REDIRECT --to-ports 5300
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+
+#OpenVPN
+iptables -t nat -I POSTROUTING -s 10.6.0.0/24 -o $CMD -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o $CMD -j MASQUERADE
+
+iptables-save >/etc/iptables/rules.v4 >/dev/null 2>&1
+iptables-save >/etc/iptables.up.rules >/dev/null 2>&1
+netfilter-persistent save >/dev/null 2>&1
+netfilter-persistent reload >/dev/null 2>&1
+systemctl enable iptables >/dev/null 2>&1 
+systemctl start iptables >/dev/null 2>&1 
+systemctl restart iptables >/dev/null 2>&1 

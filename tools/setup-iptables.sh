2654841a187d0c41d6976303f48dab6934b18124
lamtota40 <lamtota40@gmail.com>
2023-05-16 15:37:03 +0700

    Update setup-iptables.sh

--- tools/setup-iptables.sh
+++ tools/setup-iptables.sh
@@ -3,8 +3,11 @@
 ETH=$(ip -o $ETH -4 route show to default | awk '{print $5}');
 
 #SlowDNS
+iptables -A INPUT -i eth0 -p udp –dport 53 -j ACCEPT
+iptables -A INPUT -i eth0 -p udp –dport 5300 -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp –dport 2222 -j ACCEPT
 iptables -t nat -I PREROUTING -i eth0 -p udp --dport 53 -j REDIRECT --to-ports 5300
-iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+#iptables -I INPUT -p udp --dport 5300 -j ACCEPT
 
 #OpenVPN
 iptables -t nat -I POSTROUTING -s 10.6.0.0/24 -o $ETH -j MASQUERADE

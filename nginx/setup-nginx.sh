ee1b088203570617076856e6b1693b4c918ad40b
lamtota40 <lamtota40@gmail.com>
2023-05-17 21:13:18 +0700

    Update setup-nginx.sh

--- nginx/setup-nginx.sh
+++ nginx/setup-nginx.sh
@@ -4,7 +4,6 @@ apt install nginx -y
 sytemctl stop nginx
 #/var/www/html
 wget -O /etc/nginx/sites-enabled/default https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/nginx/nginx.conf
-sytemctl start nginx
-sytemctl restart nginx
+service nginx start
 
 rm -rf setup-nginx.sh

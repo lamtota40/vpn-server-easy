ea2405c686dfb023fdfbda977c3c15366eaead89
lamtota40 <lamtota40@gmail.com>
2023-05-18 13:34:46 +0700

    Update setup-ramswap.sh

--- tools/setup-ramswap.sh
+++ tools/setup-ramswap.sh
@@ -7,8 +7,8 @@ sudo swapon /swapfile
 sudo cp /etc/fstab /etc/fstab.bak
 sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-sudo bash -c "echo -e 'vm.swappiness=35\nvm.vfs_cache_pressure=50' >> /etc/sysctl.conf"
-sudo sysctl vm.swappiness=35
-sudo sysctl vm.vfs_cache_pressure=50
+sudo bash -c "echo -e 'vm.swappiness=60\nvm.vfs_cache_pressure=100' >> /etc/sysctl.conf"
+sudo sysctl vm.swappiness=60
+sudo sysctl vm.vfs_cache_pressure=100
 
 rm -rf setup-ramswap.sh

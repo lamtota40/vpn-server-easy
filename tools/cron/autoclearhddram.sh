82904592a88c6a26c4217a5bd6355c1227b5b9c9
lamtota40 <lamtota40@gmail.com>
2023-05-26 18:21:40 +0700

    Update autoclearhddram.sh

--- tools/cron/autoclearhddram.sh
+++ tools/cron/autoclearhddram.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 dateis=$(date +"%m-%d-%Y/%T")
-sync; echo 1 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a
+sync; echo 1 > /proc/sys/vm/drop_caches
+swapoff -a && swapon -a
 #du -sh /var/log/*
 #truncate -s 0 /var/log/syslog
 echo "$dateis | cron ram execution" >> /root/myvpn/log/tescache.txt

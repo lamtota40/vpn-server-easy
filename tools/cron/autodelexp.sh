6d26a796bfac67fb6379131abdbee24311858ab3
lamtota40 <lamtota40@gmail.com>
2023-05-26 16:41:56 +0700

    Update autodelexp.sh

--- tools/cron/autodelexp.sh
+++ tools/cron/autodelexp.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-s_date=$(date +"%m-%d-%Y")
-s_time=$(date +"%T")
-echo "$s_date / $s_time - Sucessful Delete" >> /root/myvpn/log/logcron.txt
+dateis=$(date +"%m-%d-%Y/%T")
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/listuser.txt
+totalaccounts=`cat /tmp/listuser.txt | wc -l`
+ for((i=1; i<=$totalaccounts; i++ ))
+    do
+    tuserval=`head -n $i /tmp/listuser.txt | tail -n 1`
+    username=`echo $tuserval | cut -f1 -d:`
+    userexp=`echo $tuserval | cut -f2 -d:`
+    userexpireinseconds=$(( $userexp * 86400 ))
+    tglexp=`date -d @$userexpireinseconds`             
+    tgl=`echo $tglexp |awk -F" " '{print $3}'`
+    while [ ${#tgl} -lt 2 ]
+      do
+      tgl="0"$tgl
+    done
+    while [ ${#username} -lt 15 ]
+       do
+       username=$username" " 
+    done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$3,$4}'`
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
 usermod -L $username
 killall -u $username
-userdel -f -r $username
+userdel -f $username
+echo "$dateis | Username $username | expired : $tgl $bulantahun removed" >> /root/myvpn/log/logdelexp.txt
+fi
+done
+rm -f /tmp/listuser.txt

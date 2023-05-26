dafd0fb7c57b60ed9a8e1b4ee64a7c45c9516c7b
lamtota40 <lamtota40@gmail.com>
2023-05-26 19:48:32 +0700

    Update tes.sh

--- tools/tes.sh
+++ tools/tes.sh
@@ -7,7 +7,7 @@ a=b=0
 for PID in "${data[@]}"
 do
 #echo "check $PID";
-NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+NUM=`cat /var/log/auth.log | grep -i udp-custom | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
 USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
 IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
 if [ $NUM -eq 1 ]; then

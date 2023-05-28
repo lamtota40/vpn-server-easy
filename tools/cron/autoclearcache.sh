cd4f8d791d075f555c18c77d293402eb4b070f63
lamtota40 <lamtota40@gmail.com>
2023-05-28 16:12:42 +0700

    Update autoclearcache.sh

--- tools/cron/autoclearcache.sh
+++ tools/cron/autoclearcache.sh
@@ -4,16 +4,12 @@ dateis=$(date +"%m-%d-%Y/%R")
 sync; echo 1 > /proc/sys/vm/drop_caches
 swapoff -a && swapon -a
 journalctl --vacuum-size=500M
-#du -sh /var/log/*
-#truncate -s 0 /var/log/syslog
-
+#Delete file log for > 1GB
 count=$(du -bs /var/log/* | awk '$1 >= 1*(1024*1024*1024)' | wc -l)
 for (( i=1 ; i<=$count ; i++ )); 
 do
-    echo $i
     file=$(du -bs /var/log/* | awk '$1 >= 1*(1024*1024*1024)' | awk '{print $2}' | awk 'NR==1')
     truncate -s 0 $file
 done
-
 #truncate -s 0 $file
 echo "$dateis | Execution Cron Clear Cache RAM/SWAP/HDDlog" >> /root/myvpn/log/logcron.txt

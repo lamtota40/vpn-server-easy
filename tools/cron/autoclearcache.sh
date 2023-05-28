934f5fe4822892fc25ce42b1079f1854afb04cdd
lamtota40 <lamtota40@gmail.com>
2023-05-28 16:19:23 +0700

    Update autoclearcache.sh

--- tools/cron/autoclearcache.sh
+++ tools/cron/autoclearcache.sh
@@ -3,7 +3,6 @@
 dateis=$(date +"%m-%d-%Y/%R")
 sync; echo 1 > /proc/sys/vm/drop_caches
 swapoff -a && swapon -a
-journalctl --vacuum-size=500M
 #Delete file log for more 1GB
 count=$(du -bs /var/log/* | awk '$1 >= 1*(1024*1024*1024)' | wc -l)
 for (( i=1 ; i<=$count ; i++ )); 

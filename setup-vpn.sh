176e952f0ffba76015e629a52df7e3f3fc7bf378
lamtota40 <lamtota40@gmail.com>
2023-05-29 21:01:08 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -21,7 +21,7 @@ echo $domain > /root/myvpn/nsdomain
 
 #dependency
 #apt install python jq cron curl openssl iptables iptables-persistent net-tools -y
-apt install gcc make iptables python jq cron curl openssl net-tools unzip -y
+apt install gcc make iptables python jq cron curl openssl net-tools unzip lolcat -y
 
 site="https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main"
 

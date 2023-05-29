7e5eaeab85f0146ddfb5aa268a3cbe59c3499e4b
lamtota40 <lamtota40@gmail.com>
2023-05-29 07:26:39 +0700

    Update autoreboot.sh

--- tools/cron/autoreboot.sh
+++ tools/cron/autoreboot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dateis=$(date +"%m-%d-%Y/%R")
-echo "$dateis | Server sucessful reboot" >> /root/myvpn/log/logcron.txt
 truncate -s 0 /var/log/auth.log
 journalctl --vacuum-size=500M
+echo "$dateis | Server sucessful reboot" >> /root/myvpn/log/logcron.txt
 /sbin/shutdown -r now

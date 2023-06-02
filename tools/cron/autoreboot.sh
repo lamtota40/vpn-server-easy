7b3cf7f81131649c1a92afe80a9f93a48754d3e8
lamtota40 <lamtota40@gmail.com>
2023-06-02 14:47:16 +0700

    Update autoreboot.sh

--- tools/cron/autoreboot.sh
+++ tools/cron/autoreboot.sh
@@ -2,6 +2,7 @@
 
 dateis=$(date +"%d-%m-%Y/%R")
 truncate -s 0 /var/log/auth.log
+truncate -s 0 /var/log/syslog
 /root/myvpn/cron/autoclearcache
 journalctl --vacuum-size=100M
 echo "$dateis | Server sucessful reboot" >> /root/myvpn/log/logcron.txt

644a77277422f4a1265202baec36022dfcbb2034
lamtota40 <lamtota40@gmail.com>
2023-06-04 13:43:02 +0700

    Update setup-sslh2.sh

--- sslh/setup-sslh2.sh
+++ sslh/setup-sslh2.sh
@@ -1,5 +1,5 @@
 
-apt instal make libconfig-dev libwrap0-dev libsystemd-dev libcap-dev libbsd-dev libev-dev git -y
+apt instal make libconfig-dev libwrap0-dev libsystemd-dev libcap-dev libbsd-dev libev-dev libpcre2-dev git -y
 git clone https://github.com/yrutschle/sslh.git
 cd sslh
 make

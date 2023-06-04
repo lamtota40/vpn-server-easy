08692743cee06b3930a0ea4e34d17677a7aa0982
lamtota40 <lamtota40@gmail.com>
2023-06-04 15:28:54 +0700

    Create setup-sslh3.sh

---
+++ sslh/setup-sslh3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#SSLH support multiplexer TCP UDP & Probes for HTTP, TLS/SSL (including SNI and ALPN), SSH, OpenVPN, tinc, XMPP, SOCKS5,
+
+DEBIAN_FRONTEND=noninteractive apt-get install sslh --no-install-recommends -y
+wget -O /etc/default/sslh "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.conf"
+mkdir /etc/sslh
+wget -O /etc/sslh/sslh.cfg "https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/sslh/sslh.cfg"
+service sslh restart

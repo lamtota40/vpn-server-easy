19e52fca7b2004c27db4080bcbdbc9385d686806
lamtota40 <lamtota40@gmail.com>
2023-06-04 09:26:31 +0700

    Update setup-websocket.sh

--- websocket/setup-websocket.sh
+++ websocket/setup-websocket.sh
@@ -4,12 +4,12 @@ if [ ! $(which python) ]; then
    apt install python -y
 fi
 
-#instalasi Websocket (accept http port 2082 forward to port 23[dropbear])
-wget -O /usr/local/bin/ws-openvpn82 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-openvpn82.py && chmod +x /usr/local/bin/ws-openvpn82
+#instalasi Websocket (accept http port 2052 forward to port 1194[OpenVPN])
+wget -O /usr/local/bin/ws-openvpn2052 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-openvpn2052.py && chmod +x /usr/local/bin/ws-openvpn2052
 
-cat > /etc/systemd/system/ws-openvpn82.service <<-END
+cat > /etc/systemd/system/ws-openvpn2052.service <<-END
 [Unit]
-Description=HTTP Websocket Over OpenVPN 82 Python
+Description=HTTP Websocket Over OpenVPN 2052 Python
 Documentation=https://github.com/lamtota40
 After=network.target nss-lookup.target
 
@@ -20,17 +20,17 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Restart=on-failure
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-openvpn82
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-openvpn2052
 
 [Install]
 WantedBy=multi-user.target
 END
-chmod +x /etc/systemd/system/ws-http2082.service
+chmod +x /etc/systemd/system/ws-openvpn2052.service
 ###################################################
-#instalasi Websocket (accept http port 2082 forward to port 23[dropbear])
-wget -O /usr/local/bin/ws-http2082 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-http2082.py && chmod +x /usr/local/bin/ws-http2082
+#instalasi Websocket (accept http port 2082 forward to port 22[OpenSSH])
+wget -O /usr/local/bin/ws-ssh2082 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-ssh2082.py && chmod +x /usr/local/bin/ws-ssh2082
 
-cat > /etc/systemd/system/ws-http2082.service <<-END
+cat > /etc/systemd/system/ws-ssh2082.service <<-END
 [Unit]
 Description=HTTP Websocket Over SSH 2082 Python
 Documentation=https://github.com/lamtota40
@@ -43,19 +43,18 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Restart=on-failure
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-http2082
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ssh2082
 
 [Install]
 WantedBy=multi-user.target
 END
-chmod +x /etc/systemd/system/ws-http2082.service
+chmod +x /etc/systemd/system/ws-dropbear8880.service
 ###################################################
-#instalasi Websocket (accept http port 8880 forward to port 23[dropbear])
-wget -O /usr/local/bin/ws-http8880 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-http8880.py && chmod +x /usr/local/bin/ws-http8880
+wget -O /usr/local/bin/ws-ssh2082 https://raw.githubusercontent.com/lamtota40/vpn-server-easy/main/websocket/ws-ssh2082.py && chmod +x /usr/local/bin/ws-ssh2082
 
-cat > /etc/systemd/system/ws-http8880.service <<-END
+cat > /etc/systemd/system/ws-ssh2082.service <<-END
 [Unit]
-Description=HTTP Websocket Over SSH 8880 Python
+Description=HTTP Websocket 2082 over OpenSSH 22 Python
 Documentation=https://github.com/lamtota40
 After=network.target nss-lookup.target
 
@@ -66,12 +65,12 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 Restart=on-failure
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-http8880
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ssh2082
 
 [Install]
 WantedBy=multi-user.target
 END
-chmod +x /etc/systemd/system/ws-http8880.service
+chmod +x /etc/systemd/system/ws-ssh2082.service
 ##################################################
 systemctl daemon-reload
 systemctl enable ws-openvpn82.service

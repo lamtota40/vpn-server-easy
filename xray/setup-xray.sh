8aab82b1b290feff1dff71e60aeea11461ea532a
lamtota40 <lamtota40@gmail.com>
2023-06-05 19:08:57 +0700

    Update setup-xray.sh

--- xray/setup-xray.sh
+++ xray/setup-xray.sh
@@ -2,9 +2,9 @@
 
 domain=$(cat /root/myvpn/domain)
 cp /root/myvpn/domain /etc/xray/domain
-#apt install iptables iptables-persistent -y
+#apt install cron iptables iptables-persistent -y
 apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
-apt install socat cron bash-completion ntpdate -y
+apt install socat bash-completion ntpdate -y
 ntpdate pool.ntp.org
 apt -y install chrony
 timedatectl set-ntp true

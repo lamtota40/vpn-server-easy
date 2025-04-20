e1590bcfb659c2c50b4273e3800f23f63357a483
lamtota40 <lamtota40@gmail.com>
2025-04-20 09:13:58 +0700

    Update backup-import.sh

--- tools/backup-import.sh
+++ tools/backup-import.sh
@@ -26,3 +26,30 @@ for user in $(awk -F: '$3 >= 1000 {print $1}' /etc/passwd); do
 done
 
 echo "Export completed. Data saved to $OUTPUT_FILE."
+
+
+INPUT_FILE="data-users.txt"
+
+# Membaca file baris per baris
+while IFS=: read -r username password expire blocked shell; do
+    # Membuat pengguna baru
+    sudo useradd -m -s "$shell" "$username"
+
+    # Mengatur password
+    echo "$username:$password" | sudo chpasswd
+
+    # Mengatur tanggal kedaluwarsa
+    if [ "$expire" != "never" ]; then
+        sudo chage -E "$expire" "$username"
+    fi
+
+    # Mengatur status blokir
+    if [ "$blocked" == "Yes" ]; then
+        sudo usermod -L "$username"
+    else
+        sudo usermod -U "$username"
+    fi
+
+done < "$INPUT_FILE"
+
+echo "Import completed."

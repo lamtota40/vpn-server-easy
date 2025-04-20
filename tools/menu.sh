89fb7cdf678188894fd7b3c401797ae62bf665ae
lamtota40 <lamtota40@gmail.com>
2025-04-20 19:49:42 +0700

    Update menu.sh

--- tools/menu.sh
+++ tools/menu.sh
@@ -46,6 +46,14 @@ OS_NAME=$(lsb_release -si)
 OS_VERSION=$(lsb_release -sr)
 OS_CODENAME=$(lsb_release -sc)
 ARCHITECTURE=$(uname -m)
+prosesor_info=$(cat /proc/cpuinfo | grep 'vendor_id' | uniq)
+if [[ $prosesor_info == *"GenuineIntel"* ]]; then
+    pinfo="INTEL"
+elif [[ $prosesor_info == *"AuthenticAMD"* ]]; then
+    pinfo= "AMD"
+else
+    pinfo="UNKNOWN"
+fi
 
 echo -e "\e[36m╒══════════════════════════════════════════════════╕\033[0m"
 echo -e " \E[46;1;37m                    INFO SERVER                   \E[0m"
@@ -56,7 +64,7 @@ echo -e "${PINK}OS${NC}       :${BLUE} $OS_NAME | $OS_VERSION $OS_CODENAME | $AR
 echo -e "${PINK}Host${NC}	 :${BLUE} $domain | $(jq -r '.query' <<< "$cekip")${NC}"
 echo -e "${PINK}NS Host${NC}	 :${BLUE} $nsdomain ${NC}"
 echo -e "${PINK}ISP${NC}	 :${BLUE} $(jq -r '.isp' <<< "$cekip") | $(jq -r '.country' <<< "$cekip") ${NC}"
-echo -e "${PINK}CPU Core${NC} :${BLUE} $cpucore CORE/VCPU ${NC}"
+echo -e "${PINK}CPU Info${NC} :${BLUE} $cpucore CORE/VCPU | $pinfo ${NC}"
 echo -e "${PINK}CPU${NC}	 :${BLUE} Usage = ${YEL}"$cpuuse"% ${BLUE}| C.Speed = ${YEL}"$cpuinfo" MHz ${NC}"
 echo -e "${PINK}RAM${NC}	 :${BLUE} Usage = ${YEL}"$ram_usage" Mb ($(($ram_usage * 100 / $ram_total))%) ${BLUE}| Total = ${YEL}"$ram_total" Mb ${NC}"
 echo -e "${PINK}SWAP RAM${NC} :${BLUE} Usage = ${YEL}"$swap_usage" Mb ("$swap_percent"%) ${BLUE}| Total = ${YEL}"$swap_total" Mb ${NC}"

97cf471d84f1074a4052238ae130c7788b69cca7
lamtota40 <lamtota40@gmail.com>
2025-04-22 13:33:25 +0700

    Update detailserver.sh

--- tools/detailserver.sh
+++ tools/detailserver.sh
@@ -6,14 +6,6 @@ if [ ! $(which jq) ]; then
 fi
 
 clear
-os_name=$(awk -F= '$1=="NAME" { print $2 ;}' /etc/os-release)
-os_version=$(awk -F= '$1=="VERSION" { print $2 ;}' /etc/os-release)
-raminfo=$(free -h | grep Mem)
-core=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
-freq=$(awk -F: '/cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
-#free -m
-ram_free_p=$(free | grep Mem | awk '{print $4/$2 * 100}')
-hddinfo=$(df -h --total | grep total)
 private_ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n "$ip_number"p)
 cekip=$(curl -s "https://get.geojs.io/v1/ip/geo.json")
 swapinfo=$(swapon --bytes)
@@ -28,20 +20,10 @@ echo "Hostname = $HOSTNAME"
 echo "Ip Public IPV4= $(wget -4qO- ipinfo.io/ip) "
 echo "Ip Public IPv6= $(jq -r '.ip' <<< "$cekip") "
 echo "Ip Private = $private_ip"
-echo "ISP = $(jq -r '.organization_name' <<< "$cekip") | Country= $(jq -r '.country' <<< "$cekip")"
-echo "Region= $(jq -r '.region' <<< "$cekip") | City= $(jq -r '.city' <<< "$cekip")"
 echo "Virtualization = " `if grep -Eoc '(vmx|svm)' /proc/cpuinfo; then echo "(enable)"; else echo "(disable)"; fi`
-echo "Type = $(systemd-detect-virt)"
-echo "Architecture = $(uname -m)"
-echo "OS = $os_name $os_version"
 echo "Frimware = $([ -d /sys/firmware/efi ] && echo UEFI || echo BIOS)"
-echo "Number of core = $core"
-echo "CPU frequency = $freq MHz"
 echo "Model name = $(lscpu | grep -oP "(?<=Model name:).*" | sed 's/^[ \t]*//;s/[ \t]*$//')"
 echo "Kernel = $(hostnamectl | grep -oP "(?<=Kernel:).*")"
-echo "RAM Free = $(awk '{print $4}' <<< "$raminfo") (${ram_free_p%.*} %) | Usage = $(awk '{print $3}' <<< "$raminfo") | Total = $(awk '{print $2}' <<< "$raminfo")"
-echo "SWAP Free = $(awk 'FNR == 2 {print$3-$4}' <<< "$swapinfo" | numfmt --to=iec) (${ram_free_p%.*} %) | Usage = $(awk 'FNR == 2 {print$4}' <<< "$swapinfo" | numfmt --to=iec) | Total = $(awk 'FNR == 2 {print$3}' <<< "$swapinfo" | numfmt --to=iec)"
-echo "HDD Free = $(awk '{print $4}' <<< "$hddinfo") | Usage = $(awk '{print $3}' <<< "$hddinfo") | Total = $(awk '{print $2}' <<< "$hddinfo")"
 echo "========================================"
 parted -l
 echo "========================================"

56a75bd3a5b5fdf6254d437c1afa0a6223c0df5c
lamtota40 <lamtota40@gmail.com>
2025-04-30 14:34:36 +0700

    Update setup-vpn.sh

--- setup-vpn.sh
+++ setup-vpn.sh
@@ -6,7 +6,7 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
-if systemctl list-units --type=service | grep -q screen; then
+if command -v screen >/dev/null 2>&1; then
 if screen -list | grep -q "\.setupvpn"; then
     echo "Sesi 'setupvpn' sudah berjalan. Menyambung kembali..."
     clear
